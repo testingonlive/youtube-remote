c9fb0f4015a1ac694be72489b09ba6186aeb2f65
jonny5 <e_t@mail.com>
2015-02-05 22:21:22 +0000

    scruffy code for multiple remotes and videos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require( 'express' )();
 var http = require( 'http' ).Server( app );
 var io = require( 'socket.io' )( http );
+var randWord = require( 'random-word-by-length' );
 
 app.get( '/', function( req, res ){
     res.sendFile( __dirname + '/video/index.html' ); 
@@ -10,11 +11,48 @@ app.get( '/remote', function( req, res ){
     res.sendFile( __dirname + '/remote/index.html' );
 })
 
+
+var assignRoom = (function(){
+     var _rooms = [];
+    
+    return function _assignRoom(){
+        var room = randWord( 4 );
+        
+        if( !~_rooms.indexOf( room ) ) {
+            _rooms.push( room );
+            return room;
+        } else {
+            return _assignRoom();
+        }        
+    }
+    
+    
+}())
+
+
 io.on( 'connection', function( socket ){
     
+    var _room;
+    
+    socket.on( 'joinRoom', function( room ){
+       socket.join( room );
+        
+        _room = room;
+    });
+    
+    socket.on( 'requestRoom', function( callback ){
+        _room = assignRoom();
+               
+        socket.join( _room )
+        
+        callback( _room );
+    });
+    
+    
     socket.on( 'start', function(){
-        socket.broadcast.emit( 'start' );
-    })
+        socket.to( _room ).broadcast.emit( 'start' );
+    });
+    
     
     console.log( 'a user connected' );
     
